30c91716040b620ea79e2d63b1d5fa1758ac6b8a
RyoUematsU <ryouematsu77@gmail.com>
2024-08-29 08:42:58 +0900

    modal1

--- src/components/organisms/user/UserCard.tsx
+++ src/components/organisms/user/UserCard.tsx
@@ -5,12 +5,13 @@ type Prosps = {
   imageUrl: string;
   userName: string;
   fullName: string;
+  onClick: () => void;
 }
 
 export const UserCard: FC<Prosps> = memo((props) => {
-  const {imageUrl, userName, fullName} = props;
+  const {imageUrl, userName, fullName, onClick} = props;
   return (
-    <Box w="260px" h="260px" bg="white" borderRadius="10px" shadow="md" p={4} _hover={{cursor:"pointer", opacity:0.8}}>
+    <Box w="260px" h="260px" bg="white" borderRadius="10px" shadow="md" p={4} _hover={{cursor:"pointer", opacity:0.8}} onClick={onClick}>
       <Stack textAlign="center">
         <Image boxSize="160px" borderRadius="full" src={imageUrl} alt="プロフィール画像" m="auto"/>
         <Text fontSize="lg" fontWeight="bold">{userName}</Text>
--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,10 +1,13 @@
-import { Center, Spinner, Wrap, WrapItem } from "@chakra-ui/react";
-import {memo, FC, useEffect} from "react"
+import { Center, Spinner, useDisclosure, Wrap, WrapItem } from "@chakra-ui/react";
+import {memo, FC, useEffect, useCallback} from "react"
 import { UserCard } from "../organisms/user/UserCard";
 import { useAllUsers } from "../../hooks/useAllUsers";
+import { UserDetailModal } from "../organisms/user/UserDetailModal";
 
 export const UserManegement: FC = memo(() => {
+  const {isOpen, onClose, onOpen} = useDisclosure();
   const {getUsers, users, loading} = useAllUsers();
+  const onClickuser = useCallback(() => onOpen(),[]);
 
 useEffect(() => getUsers(), []);
 
@@ -22,13 +25,50 @@ useEffect(() => getUsers(), []);
             <UserCard
               imageUrl="https://source.unsplash.com/random"
               userName={user.username}
-              fullName={user.name}/>
+              fullName={user.name}
+              onClick={onClickuser}/>
           </WrapItem>)
         )}
 
       
     </Wrap>
     )}
+    <UserDetailModal isOpen={isOpen} onClose={onClose} />
+    {/* <Modal isOpen={isOpen} onClose={onClose} autoFocus={false} motionPreset="slideInBottom">
+      <ModalOverlay />
+        <ModalContent pb={4}>
+          <ModalHeader>ユーザー詳細</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody mx={4}>
+            <Stack spacing={4}>
+              <FormControl>
+                <FormLabel>
+                  名前
+                </FormLabel>
+                <Input value="jakee" isReadOnly />
+              </FormControl>
+              <FormControl>
+                <FormLabel>
+                  フルネーム
+                </FormLabel>
+                <Input value="jakee" isReadOnly />
+              </FormControl>
+              <FormControl>
+                <FormLabel>
+                  Mail
+                </FormLabel>
+                <Input value="jakee" isReadOnly />
+              </FormControl>
+              <FormControl>
+                <FormLabel>
+                  電話番号
+                </FormLabel>
+                <Input value="jakee" isReadOnly />
+              </FormControl>
+            </Stack>
+          </ModalBody>
+        </ModalContent>
+    </Modal> */}
     </>
   )
 });
